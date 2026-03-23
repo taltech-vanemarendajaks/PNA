783acbfc89fc9f660b395880ba1a1969e6d44d26
Carina Ruut <carinaruut@gmail.com>
2026-03-23 21:54:26 +0200

    PNA-6: fix: Add heroicons support, separate Alert component

--- pna-web/src/components/search-component/SearchComponent.tsx
+++ pna-web/src/components/search-component/SearchComponent.tsx
@@ -1,5 +1,6 @@
 import { type ChangeEvent, useState } from "react";
 import { validatePhoneNumber } from "./SearchComponent.validation";
+import { Alert } from "../common/Alert";
 
 export function SearchComponent() {
   const [phoneNumber, setPhoneNumber] = useState("");
@@ -27,25 +28,7 @@ export function SearchComponent() {
     <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-full border p-4">
       <legend className="fieldset-legend text-2xl text-primary">Search</legend>
 
-      {error ? (
-        <div role="alert" className="alert alert-error mb-4">
-          <svg
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6 shrink-0 stroke-current"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-          </svg>
-          <span>{error}</span>
-        </div>
-      ) : null}
+      {error ? <Alert type="error" message={error} /> : null}
 
       <input
         type="tel"
